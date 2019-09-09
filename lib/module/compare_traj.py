3aaef6e57d8078b7bb55bf8fcccc9342e809e6de
k <ka2hyeon@gmail.com>
2019-09-09 09:47:46 +0900

    master

--- lib/module/compare_traj.py
+++ lib/module/compare_traj.py
@@ -303,7 +303,7 @@ def compare(config):
 
                 ax.clear()
                 obj_vicon.apply_pose(vicon_se3_t)
-                obj_vision.apply_pose(vision_T_t)
+                obj_vision.apply_pose(vision_se3_t)
                 obj_vicon.plot(ax, scale = 0.015, linewidth = 3)
                 obj_vision.plot(ax, scale = 0.015, linewidth = 3)
                 ##
