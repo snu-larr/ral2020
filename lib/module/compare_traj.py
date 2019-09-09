a1b756cc9991283cce6bec4887652cb49856f1f7
k <ka2hyeon@gmail.com>
2019-09-09 20:54:06 +0900

    master

--- lib/module/compare_traj.py
+++ lib/module/compare_traj.py
@@ -210,7 +210,7 @@ def compare(config):
             plt.close('all')
             ## align translation
             x0 = np.random.rand(6)
-            optimize_len = int(len(vision_plot)*0.3)
+            optimize_len = int(len(vision_plot))
             ########################## for task3 (occlusion)
             #if task_name == 'task3':
             #    optimize_len = 100
